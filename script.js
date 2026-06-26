810b751d3a48f425b07dca9628bf5d718e96e2f1
MinervaVermillion <atulen2007@gmail.com>
2026-06-26 20:03:53 +0700

    Portfolio web, navbar

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const burgerBtn = document.getElementById('burgerBtn');
-const navMenu = document.getElementById('navMenu');
+const btn = document.getElementById('theme-btn');
+
+btn.addEventListener('click', function() {
+    document.body.classList.toggle('dark');
+
+    const icon = btn.querySelector('i');
+    if (document.body.classList.contains('dark')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
+});
+
+const burgerBtn = document.getElementById('burger-btn');
+const navMenu = document.querySelector('ul');
 
 burgerBtn.addEventListener('click', function() {
     navMenu.classList.toggle('open');
-    burgerBtn.classList.toggle('active');
 });
-
-document.addEventListener('click', function(event) {
-    const navbar = document.querySelector('.navbar');
-    if (!navbar.contains(event.target)) {
-        navMenu.classList.remove('open');
-        burgerBtn.classList.remove('active');
-    }
-    }
-);
